a2f30196c8f7586c8a8b50104a20eaed630de144
Mohamed Ashraf <93843532+Mohamedragih1@users.noreply.github.com>
2024-03-03 11:50:22 +0200

    Update 8puzzles.py
    DFS implementation first trial

--- 8puzzles.py
+++ 8puzzles.py
@@ -1,3 +1,6 @@
+from collections import deque
+import random
+
 class BoardState:
     def __init__(self, current_board:list, prev_state:'BoardState', prev_action):
         self.current_board = current_board
@@ -23,34 +26,38 @@ class BoardState:
         else:
             return True
     
-    def takeAction(self, action):
-       
-        # Check if valid move    
-        if self.checkAction(action) == False:
+
+    def takeAction(self, action): 
+        if not self.checkAction(action):
             return None
+
+        new_board = [row[:] for row in self.current_board]
         
-        new_board = self.current_board.copy()
-        
+        zero_row, zero_col = self.zero_index
+
         if action == 'U':
-            new_board[self.zero_index[0]][self.zero_index[1]], new_board[self.zero_index[0] - 1][self.zero_index[1]]  = new_board[self.zero_index[0] - 1][self.zero_index[1]], new_board[self.zero_index[0]][self.zero_index[1]] 
+            new_board[zero_row][zero_col], new_board[zero_row - 1][zero_col] = new_board[zero_row - 1][zero_col], 0
         elif action == 'D':
-            new_board[self.zero_index[0]][self.zero_index[1]], new_board[self.zero_index[0] + 1][self.zero_index[1]]  = new_board[self.zero_index[0] + 1][self.zero_index[1]], new_board[self.zero_index[0]][self.zero_index[1]] 
+            new_board[zero_row][zero_col], new_board[zero_row + 1][zero_col] = new_board[zero_row + 1][zero_col], 0
         elif action == 'L':
-            new_board[self.zero_index[0]][self.zero_index[1]], new_board[self.zero_index[0]][self.zero_index[1] - 1]  = new_board[self.zero_index[0]][self.zero_index[1] - 1], new_board[self.zero_index[0]][self.zero_index[1]] 
+            new_board[zero_row][zero_col], new_board[zero_row][zero_col - 1] = new_board[zero_row][zero_col - 1], 0
         elif action == 'R':
-            new_board[self.zero_index[0]][self.zero_index[1]], new_board[self.zero_index[0]][self.zero_index[1] + 1]  = new_board[self.zero_index[0]][self.zero_index[1] + 1], new_board[self.zero_index[0]][self.zero_index[1]] 
-                
+            new_board[zero_row][zero_col], new_board[zero_row][zero_col + 1] = new_board[zero_row][zero_col + 1], 0
+                    
         new_state = BoardState(new_board, self, action)
 
         return new_state
+
     
     def getAllAction(self):
         available_actions = []
         for i in ['U', 'D', 'L', 'R']:
             if self.checkAction(i) == True:
                 available_actions.append(i)
-        
-        return i
+
+        random.shuffle(available_actions)
+
+        return available_actions
     
     def printState(self):
         for i in range(3):
@@ -66,22 +73,59 @@ def finished(state:BoardState):
                 return False
     
     return True
-    
+
+def isEmpty(stack):
+    if not stack:
+        return True
+    else:
+        return False
+
+
+def DFS(init_board):
+    init_state = BoardState(init_board, None, None)
+    frontier = deque()
+    explored = set()
+    frontier.append(init_state)
+    count = 0
+    while not isEmpty(frontier):
+        print()
+        print(count)
+        count += 1
+        print()
+        state = frontier.pop()
+        explored.add(state)
+        print()
+        state.printState()
         
+        if finished(state):
+            return True
+        
+        actions = state.getAllAction()
+        print()
+        print(actions)
+        print()
+        for action in actions:
+            new_state = state.takeAction(action)
+            if (new_state not in frontier) and (new_state not in explored):
+                frontier.append(new_state)
+                
+    return False
+
+
 def main():
     
     init_board = [
-        [4, 1, 2],
-        [3, 0, 5],
+        [1, 0, 2],
+        [3, 4, 5],
         [6, 7, 8]
     ]
     
     init_state = BoardState(init_board, None, None)
     init_state.printState()
     print()
-    state2 = init_state.takeAction('D')
-    state2.printState()
     
+   
+    print(DFS(init_board))
     
 if __name__ == "__main__":
     main()
